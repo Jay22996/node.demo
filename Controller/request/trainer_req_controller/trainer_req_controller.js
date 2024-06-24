4c3d1b564a9c2e7a9fffe4c6989b4979f63b1ed7
Jay22996 <jay00289@gmail.com>
2024-06-24 11:18:37 +0530

    done requ...

--- Controller/request/trainer_req_controller/trainer_req_controller.js
+++ Controller/request/trainer_req_controller/trainer_req_controller.js
@@ -1,17 +1,21 @@
+const { sendreq, acceptreq, showreq, delete_reqq } = require("../../../Repository/request_repo/trainer_req_repo/trainer_req_repo");
 
 
 exports.sendreq = async (req, res) => {
-  
+    sendreq(req, res)
 };
 
 exports.acceptreq = async (req, res) => {
+    acceptreq(req, res)
  
 };
 
 exports.showreq = async (req, res) => {
+    showreq(req, res)
   
 };
 
 exports.delete_reqq = async (req, res) => {
+    delete_reqq(req, res)
   
 };
